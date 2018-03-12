70e9e4946e979482967357016693f329661dc23a
dawidozga <dawidozga93@gmail.com>
2018-03-12 20:37:37 +0100

    fourth commit

---
+++ ConsoleApplication1/ConsoleApplication1/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class User
+    {
+        private string name;
+        private string surname;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
